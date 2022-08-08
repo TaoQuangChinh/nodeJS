c3a085c16c051c21b90eb59b5d1bbbc056de5449
ChinhTQ <chinhtq@saisystem.vn>
2022-08-08 17:39:25 +0700

    update

--- project_nodejs_02/routes.js
+++ project_nodejs_02/routes.js
@@ -1,36 +1,74 @@
 const fs = require('fs');
+const nodemailer = require('./node_modules/nodemailer');
+const uuid = require('uuid');
+var body = [];
+
+const myEmail = nodemailer.createTransport({
+    service: 'gmail',
+    auth:{
+        user: 'chinhtao1908@gmail.com',
+        pass: 'taoquangchinh1908'
+    }
+});
+
+const emailOption = (toMail,name) =>{
+    const random = uuid.v4().split('-');
+    return {
+        from: 'chinhtao1908@gmail.com',
+        to: toMail,
+        subject:'Mật khẩu mặc định',
+        text: `Xin chào ${name},\nChào mừng bạn đến với cờ caro.\nĐây là mật khẩu do chúng tôi cung cấp để truy cập vào game ${random[4]}.\nLưu ý: sau khi truy cập vào trò chơi, bạn đổi mật khẩu ở phần cài đặt`
+    };
+};
 
 //res: server phản hồi tới đối tượng(data server trả về cho đối tượng)
 //req: đối tượng phản hồi tới server(data đối tượng gửi lên server)
-const handleRequestListener = (req,res)=>{
+const handleRequestListener = (req, res) => {
     var url = req.url;
-    const method = req.method;
-    if(url === '/'){
-        res.setHeader('Content-type','form');
-    res.write('<html>');
-    res.write('<body><form action="/mess" method="POST"><input type="text" name="maessage"><button type="submit">SEND</button></form></body>');
-    res.write('</html>');
-    return res.end();
-    }
-    if(url === '/mess' && method === "POST"){
-        const body = [];
-        req.on('data',(chunk)=>{
-            body.push(chunk);
-        });
-        return req.on('end',()=>{
-            const parsedBody = Buffer.concat(body).toString();
-            const mess = parsedBody.split('=')[1];
-            fs.writeFile('data_login.txt',mess,(err)=>{
-                res.statusCode = 302;
-                res.setHeader('Location','/');
-                return res.end();
+
+    req.on('data', (chunk) => {
+        body.push(chunk);
+    });
+    return req.on('end', () => {
+        const parsedBody = Buffer.concat(body).toString();
+        const { id, name, pass, name_game } = JSON.parse(parsedBody);
+        if (url === '/login') {
+            const result =  dataRegister("",name,pass,"");
+            handleWriteFile('data_login.txt', JSON.stringify(result));
+        } else if (url === '/register') {
+            const result =  dataRegister(id,name,pass,name_game);
+            handleWriteFile('data_register.txt', JSON.stringify(result));
+            myEmail.sendMail(emailOption(`${name}`,`${name_game}`),(err,info)=>{
+                if(err){
+                    return console.log(err);
+                }else{
+                    
+                }
             });
-        });
-    }
-    return res.end();
+        }
+        body = [];
+    });
+};
+
+const dataRegister = (id,name,pass,nameGame) =>{
+    return {
+        id: id,
+        userName: name,
+        passWord: pass,
+        name_game: nameGame
+    };
 };
 
 const logText = 'Test log...';
+
+const handleWriteFile = (nameFile, dataFile) => fs.writeFile(nameFile, dataFile,
+    (err) => {
+        if (err) {
+            res.statusCode = 404;
+            res.write("404 Not Found");
+            return res.end();
+        }
+    });
 
 //Method 1
 // exports.handle = handleRequestListener;
