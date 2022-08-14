68b4c85d8c676643243f910a0ef62474fb3a13d9
TaoQuangChinh <91710008+TaoQuangChinh@users.noreply.github.com>
2022-08-14 23:24:04 +0700

    update server

--- project_nodejs_02/app.js
+++ project_nodejs_02/app.js
@@ -1,76 +1,61 @@
-const http = require('http');
 const string = require('./common/string');
 const nodemailer = require('./node_modules/nodemailer');
 const randomstring = require('./node_modules/randomstring');
 var db = require('./db/database');
 const express = require('express');
 const apps = express();
+const bodyParser = require('body-parser');
 
-var body = [];
+apps.use(bodyParser.urlencoded({ extended: true }));
+apps.use(bodyParser.json());
 
 apps.post("/login", (req, res) => {
-    req.on('data', (chunk) => {
-        body.push(chunk);
-    });
-    return req.on('end', () => {
-        const parsedBody = Buffer.concat(body).toString();
-        const { email, pass } = JSON.parse(parsedBody);
-        db.execute('SELECT email, pass FROM user').then(data => {
-            for (let arr of data[0]) {
-                if (arr['email'] === email && arr['pass'] === pass) {
-                    res.statusCode = 200;
-                    return res.json({
-                        code: 0,
-                        message: "đăng nhập thành công!",
-                        payload: null
-                    });
-                } else {
-                    return res.json({
-                        code: 400,
-                        message: "tài khoản email hoặc mật khẩu sai.",
-                        payload: null
-                    });
-                }
-            }
-        }).catch(err => {
-            console.log(err);
-            return res.json(string.jsonErr202);
-        });
-        body = [];
+    const { email, pass } = req.body;
+    db.execute('SELECT * FROM user WHERE email = ? AND pass = ?', [email, pass]).then(data => {
+        if (data[0].length != 0) {
+            res.statusCode = 200;
+            return res.json({
+                code: 0,
+                message: "đăng nhập thành công!",
+                payload: null
+            });
+        } else {
+            return res.json({
+                code: 400,
+                message: "tài khoản email hoặc mật khẩu sai.",
+                payload: null
+            });
+        }
+    }).catch(err => {
+        console.log(err);
+        return res.json(string.jsonErr202);
     });
 });
 
 apps.post('/register', (req, res) => {
-    req.on('data', (chunk) => {
-        body.push(chunk);
+    const { id, email, nameGame, deviceMobi, images } = req.body;
+    const randomPass = randomstring.generate(7);
+    console.log(images);
+    db.execute(`INSERT INTO user VALUES (?,?,?,?,?)`, [id, email, randomPass, nameGame, deviceMobi]).then(() => {
+        res.statusCode = 200;
+        return res.json({
+            code: 0,
+            message: "tạo tài khoản thành công!",
+            payload: null
+        });
+    }).catch(err => {
+        console.log(err);
+        return res.json(string.jsonErr202);
     });
-    return req.on('end', () => {
-        const parsedBody = Buffer.concat(body).toString();
-        const { id, email, name_game, device_mobi } = JSON.parse(parsedBody);
-        const randomPass = randomstring.generate(7);
-
-        db.execute(`INSERT INTO user VALUES (?,?,?,?,?)`, [id, email, randomPass, name_game, device_mobi]).then(() => {
-            res.statusCode = 200;
+    myEmail.sendMail(emailOption(`${email}`, `${nameGame}`, `${randomPass}`), (err, info) => {
+        if (err) {
+            console.log(err);
             return res.json({
-                code: 0,
-                message: "tạo tài khoản thành công!",
+                code: 501,
+                message: "có một lỗi xảy ra trong quá trình gửi mail.",
                 payload: null
             });
-        }).catch(err => {
-            console.log(err);
-            return res.json(string.jsonErr202);
-        });
-        myEmail.sendMail(emailOption(`${email}`, `${name_game}`, `${randomPass}`), (err, info) => {
-            if (err) {
-                console.log(err);
-                return res.json({
-                    code: 501,
-                    message: "có một lỗi xảy ra trong quá trình gửi mail.",
-                    payload: null
-                });
-            }
-        });
-        body = [];
+        }
     });
 });
 
@@ -83,8 +68,8 @@ apps.get('/check-device', (req, res) => {
                 user = {
                     id: arr['id'],
                     email: arr['email'],
-                    nick_name: arr['nameGame'],
-                    mobi_device: arr['deviceMobi']
+                    nameGame: arr['nameGame'],
+                    deviceMobi: arr['deviceMobi']
                 };
             }
             break;
@@ -99,7 +84,20 @@ apps.get('/check-device', (req, res) => {
         console.log(err);
         return res.json(string.jsonErr202);
     });
-    body = [];
+});
+
+apps.get('/list-account', (req, res) => {
+    db.execute('SELECT * FROM user').then(data => {
+        res.statusCode = 200;
+        return res.json({
+            code: 0,
+            message: "lấy danh sách tài khoản thành công!",
+            payload: data[0]
+        });
+    }).catch(err => {
+        console.log(err);
+        return res.json(string.jsonErr202);
+    });
 });
 
 const myEmail = nodemailer.createTransport({
@@ -119,5 +117,5 @@ const emailOption = (toMail, name, random) => {
     };
 };
 
-const server = http.createServer(apps);
-server.listen(8000);
+apps.listen(8000, '192.168.0.104');
+module.exports = apps;
